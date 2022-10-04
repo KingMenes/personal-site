6829e5b3e9ece9cab800c6e3422d1470799d87ca
Mena <menaibr96@gmail.com>
2022-10-04 00:33:50 -0400

    Builds App.jsx

---
+++ src/app/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { useState } from 'react';
+import { ThemeProvider } from "styled-components";
+import { themes } from "./theme";
+import { GlobalStyles } from "./global"; 
+
+function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <>
+        <GlobalStyles />
+        <div>
+            <Main theme={themes[theme]} setTheme={setTheme} />
+        </div>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
