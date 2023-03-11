47ecfb7344f18815fc9a1d51137e8b31f90a4746
KingMenes <menaibr96@gmail.com>
2023-03-11 16:26:19 -0500

    added contact page

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -6,6 +6,15 @@ function ContactMe() {
       <h1 className="heading">
         <span>04. </span> Contact Me <hr />
       </h1>
+      <div className="contactme-writing">
+        <span>Get In Touch</span>
+        <p>
+          I'm currently looking for full-time Software Development or Mechanical
+          Engineering opportunities! If you know of any positions available, if
+          you have any questions, or if you just want to say hi, please feel
+          free to email me at menaibr96@gmail.com.
+        </p>
+      </div>
     </div>
   );
 }
